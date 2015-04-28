b95c41786e5966621ea17499347715a71b516031
Allen Li <allen@allen.li>
2015-04-27 23:25:54 -0700

    make some changes to usercode

--- griff/usercode.py
+++ griff/usercode.py
@@ -1,7 +1,6 @@
 # example usercode
-import api
-import random
+from api import Robot, Gamepad
 
 def teleop():
     while True:
-        api.Robot.motors[0].set_speed(random.randint(0, 25))
+        Robot.motors[0].set_speed(Gamepad.axes[0])
